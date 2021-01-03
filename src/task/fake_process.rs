3266a889a58d2dca6b413d240fcead739f942167
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-03 12:15:32 +0800

    Test FakeProcess

--- src/task/fake_process.rs
+++ src/task/fake_process.rs
@@ -3,15 +3,31 @@ pub struct FakeProcess;
 use std::io::Result;
 use crate::task::process::Process;
 
+const START: u8 = 'a' as u8;
+const END: u8 = 'z' as u8;
+
 impl Process for FakeProcess {
 	fn read(&mut self, buffer: &mut [u8]) -> Result<usize> {
-		let start = 'a' as u8;
-		let end = 'z' as u8;
-
-		for (buffer, sample) in buffer.iter_mut().zip(start..end) {
+		for (buffer, sample) in buffer.iter_mut().zip(START..END) {
 			*buffer = sample;
 		}
 
 		Ok(buffer.len())
+	}
+}
+
+#[cfg(test)]
+mod t {
+	use super::{END, FakeProcess, Process, START};
+
+	#[test]
+	fn can_read() {
+		let mut process = FakeProcess;
+		let mut buffer = [0; 20];
+
+		let result = process.read(&mut buffer);
+
+		assert_eq!(buffer.to_vec(), (START..END).take(20).collect::<Vec<u8>>());
+		assert_eq!(result.unwrap(), 20);
 	}
 }
