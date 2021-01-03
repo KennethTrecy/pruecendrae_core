a679f1c80134b29d13388ed2cf303fdbbaec9f3b
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-03 12:55:30 +0800

    Integrate FakeProcess in run_command()

--- src/task/run_command.rs
+++ src/task/run_command.rs
@@ -1,11 +1,25 @@
-use std::process::{Child, Command, Stdio};
+// When being tested, the imports at line 4, and parameters at line 5, are being marked as unused.
+// Using the attribute below, it will remove the warnings.
+#![allow(unused_imports, unused_variables)]
 
-pub fn run_command(program: String, arguments: Vec<String>) -> Child {
-	Command::new(program)
-		.args(&arguments)
-		.stdout(Stdio::piped())
-		.stdin(Stdio::piped())
-		.stderr(Stdio::piped())
-		.spawn()
-		.unwrap()
+use std::process::{Child, Command, Stdio};
+use crate::task::process::Process;
+use crate::task::fake_process::FakeProcess;
+
+pub fn run_command(program: String, arguments: Vec<String>) -> impl Process {
+	#[cfg(not(test))]
+	{
+		Command::new(program)
+			.args(&arguments)
+			.stdout(Stdio::piped())
+			.stdin(Stdio::piped())
+			.stderr(Stdio::piped())
+			.spawn()
+			.unwrap()
+	}
+
+	#[cfg(test)]
+	{
+		FakeProcess
+	}
 }
