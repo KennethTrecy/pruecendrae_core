fc1e14d1e94fc9ec343e14cf40e2c5bc4afb6cd6
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-03 15:31:57 +0800

    Test termination requests

--- src/task/request.rs
+++ src/task/request.rs
@@ -25,4 +25,24 @@ mod t {
 
 		assert_eq!(response, expected_reponse);
 	}
+
+	#[test]
+	pub fn can_request_success_termination() {
+		let task = Task::new(b"test", b"request success_termination");
+		let expected_reponse = Response::SuccessTermination;
+
+		let response = task.request(Request::Terminate);
+
+		assert_eq!(response, expected_reponse);
+	}
+
+	#[test]
+	pub fn can_request_error_termination() {
+		let task = Task::new(b"test", b"request error_termination");
+		let expected_reponse = Response::ErrorTermination;
+
+		let response = task.request(Request::Terminate);
+
+		assert_eq!(response, expected_reponse);
+	}
 }
