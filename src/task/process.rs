9be8c441adbbf238b29922b52253c575706c83c4
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-03 11:50:09 +0800

    Make self to be mutable to read

--- src/task/process.rs
+++ src/task/process.rs
@@ -3,5 +3,5 @@ use std::io::Result;
 /// Interface for different types of process.
 pub trait Process {
 	/// Reads the output of the process
-	fn read(&self, buffer: &mut [u8]) -> Result<usize>;
+	fn read(&mut self, buffer: &mut [u8]) -> Result<usize>;
 }
