a6ead538e0b85ccc2bfea593092f63594ff03a52
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-04 11:37:13 +0800

    Add a period at the documentation of Process::run

--- src/task/process.rs
+++ src/task/process.rs
@@ -2,7 +2,7 @@ use std::io::Result;
 
 /// Interface for different types of process.
 pub trait Process {
-	/// Reads the output of the process
+	/// Reads the output of the process.
 	fn read(&mut self, _: &mut [u8]) -> Result<usize>;
 
 	/// Stops the process.
