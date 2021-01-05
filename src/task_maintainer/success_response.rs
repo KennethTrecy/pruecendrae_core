a20fe61f9fd0bc9a3596323d8393b33040f23d69
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-01-05 12:43:11 +0800

    Reflect the TaskMaintainer's Request to SuccessResponse

--- src/task_maintainer/success_response.rs
+++ src/task_maintainer/success_response.rs
@@ -2,7 +2,7 @@
 pub enum SuccessResponse<'a> {
 	Output(Vec<(&'a [u8], Vec<u8>)>),
 	Start(Vec<&'a [u8]>),
-	Running(Vec<&'a [u8]>),
+	Check(Vec<&'a [u8]>),
 	Stop(Vec<&'a [u8]>),
-	Killed(Vec<&'a [u8]>)
+	Kill(Vec<&'a [u8]>)
 }
